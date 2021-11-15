0bd71ed3940aebe70811de6e2f2a02130a7eb87a
milinches <ibrogenius5165@gmail.com>
2021-11-15 04:44:43 +0100

    consecutive number solution

--- ConsecutiveNumber/consecutivenumber.go
+++ ConsecutiveNumber/consecutivenumber.go
@@ -1,2 +1,24 @@
 package main
 
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+func main() {
+	d := []int{1, 1, 0, 1, 1, 1}
+	println(findMaxConsecutiveNumber(d))
+}
+
+func findMaxConsecutiveNumber(nums []int) int {
+	max := 0
+	count := 0
+	for _, value := range nums {
+		if value == 1 {
+			count += 1
+			if max < count {
+				max = count
+			}
+		} else {
+			count = 0
+		}
+	}
+	return max
+}
